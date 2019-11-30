28d45a684e3b1f9ff323adb6cc3e6778e00c984e
John Vincent <github@tenseventyseven.uu.me>
2019-11-30 21:27:56 +0800

    Add airbnb lottie components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,18 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 import { Toast } from '@ionic-native/toast/ngx';
+import { HttpClientModule } from '@angular/common/http';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+import { IonicStorageModule } from '@ionic/storage';
+import { FormsModule } from '@angular/forms';
+import { environment } from '../environments/environment';
+import { defineCustomElements } from '@teamhive/lottie-player/loader';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, 
+  IonicStorageModule.forRoot(),
   IonicModule.forRoot({
     mode: 'md'
 }), 
@@ -25,6 +32,7 @@ import { Toast } from '@ionic-native/toast/ngx';
     Toast,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,13 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import { defineCustomElements } from '@teamhive/lottie-player/loader';
+
 if (environment.production) {
   enableProdMode();
 }
+
+defineCustomElements(window);
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
