12165de929a6b6823913bfc729874fda678fb831
John Vincent <github@tenseventyseven.uu.me>
2019-12-02 20:28:58 +0800

    Add personalization to Java activity
    Actually lol

--- android/app/src/main/java/io/tenseventyseven/arion/ArionActivity.java
+++ android/app/src/main/java/io/tenseventyseven/arion/ArionActivity.java
@@ -7,7 +7,7 @@ import com.getcapacitor.Plugin;
 
 import java.util.ArrayList;
 
-public class MainActivity extends BridgeActivity {
+public class ArionActivity extends BridgeActivity {
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
