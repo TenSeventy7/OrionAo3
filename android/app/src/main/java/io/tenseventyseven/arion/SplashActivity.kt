b5ab754e4d001eed70d3f2a63b5b7673503b89ec
John Vincent <github@tenseventyseven.uu.me>
2019-12-03 08:45:11 +0800

    Amend 91222d571a4be6a819932f6f45766ad48c039ba8
    Package name not set in Kotlin splash code, causes app to fail launch.

---
+++ android/app/src/main/java/io/tenseventyseven/arion/SplashActivity.kt
@@ -0,0 +1,29 @@
+package io.tenseventyseven.arion
+
+import android.os.Bundle
+import android.os.Handler
+import android.support.v7.app.AppCompatActivity
+
+class SplashActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        scheduleSplashScreen()
+    }
+
+    private fun scheduleSplashScreen() {
+        val splashScreenDuration = getSplashScreenDuration()
+        Handler().postDelayed(
+                {
+                    
+					val intent = Intent(this, ArionActivity::class.java)
+					startActivity(intent)
+                    finish()
+                },
+                splashScreenDuration
+        )
+    }
+
+    private fun getSplashScreenDuration() = 3000L
+
+}
