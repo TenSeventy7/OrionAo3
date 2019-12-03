911b436921ebdfa2887f1c6bdc27b3bec7a2b5c6
John Vincent <github@tenseventyseven.uu.me>
2019-12-04 06:51:13 +0800

    Use Firebase for analytics instead

--- android/app/src/main/kotlin/io/tenseventyseven/arion/SplashActivity.kt
+++ android/app/src/main/kotlin/io/tenseventyseven/arion/SplashActivity.kt
@@ -1,12 +1,5 @@
 package io.tenseventyseven.arion
 
-import com.microsoft.appcenter.AppCenter;
-import com.microsoft.appcenter.analytics.Analytics;
-import com.microsoft.appcenter.crashes.Crashes;
-import com.microsoft.appcenter.AppCenter;
-import com.microsoft.appcenter.analytics.Analytics;
-import com.microsoft.appcenter.crashes.Crashes;
-
 import android.os.Bundle
 import android.os.Handler
 import android.support.v7.app.AppCompatActivity
@@ -17,8 +10,6 @@ class SplashActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        AppCenter.start(getApplication(), "b458fa3e-c387-4728-9a2c-dcc97c3bddc8",
-                  Analytics.class, Crashes.class);
         scheduleSplashScreen()
     }
 
