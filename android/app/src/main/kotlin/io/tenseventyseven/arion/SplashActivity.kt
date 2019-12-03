8c38baae1c0ec11ee70df871eabf1764f728e20a
John Vincent <github@tenseventyseven.uu.me>
2019-12-03 11:03:53 +0800

    Fix unresolved reference in Kotlin code

--- android/app/src/main/kotlin/io/tenseventyseven/arion/SplashActivity.kt
+++ android/app/src/main/kotlin/io/tenseventyseven/arion/SplashActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.os.Handler
 import android.support.v7.app.AppCompatActivity
 
+import android.content.Intent
+
 class SplashActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
